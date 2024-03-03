fa12af6a03f8e9ff98c10a5fd367efa014c3fc05
Flaryiest <ericmzuo1@gmail.com>
2024-03-02 20:20:13 -0700

    added logic to determine game winner

--- scripts.js
+++ scripts.js
@@ -1,7 +1,40 @@
 function gameBoard (currentGameBoard) {
     if (currentGameBoard == null) 
     {currentGameBoard = [[0,0,0],[0,0,0],[0,0,0]]}
-    return {currentGameBoard}
+    board = currentGameBoard
+    function checkWin (board) {
+    
+        for (line in board) {
+            console.log(line)
+            if (board[line][0] == board[line][1] && board[line][1] == board[line][2]) {
+                if (board[line][0] == "X" || board[line][0] == "O") {
+                console.log(board[line][0], board[line][1],board[line][2], line)
+                return true
+            }
+            }
+            else if (board[0][line] == board[1][line] && board[1][line] == board[1][line]) {
+                if (board[0][line] == "X" || board[0][line] == "O") {
+                console.log(board[line][0], board[line][1],board[line][2], line)
+                return true
+                
+                }
+            }
+            else if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+                if (board[0][0] == "X" || board[0][0] == "O") {
+                    console.log("pog")
+                    return true
+                }
+            }
+            else if (board[0][2] == board[1][1] && board[1][1] == board[0][2]) {
+                if (board[0][2] == "X" || board[0][2] == "O") {
+                        console.log("pog")
+                        return true
+                }
+            }
+        }
+        return false   
+    }
+    return {currentGameBoard, checkWin}
 }
 let currentBoard = gameBoard();
 
@@ -10,6 +43,12 @@ function player (playerName, playerSymbol) {
 }
 let x = player(null, "X")
 let o = player(null, "O")
+
+function gameLogic (player) {
+    currentPlayer = player
+    return {currentPlayer}
+    
+}
 
 
 
@@ -24,6 +63,9 @@ moveInputForm.onsubmit = function(event) {
     if (currentBoard.currentGameBoard[currentMoveColumn - 1][currentMoveRow - 1] == 0) {
         currentBoard.currentGameBoard[currentMoveColumn - 1][currentMoveRow - 1] = gameSituation.currentPlayer.playerSymbol
         console.log(currentBoard.currentGameBoard)
+        if (currentBoard.checkWin(currentBoard.currentGameBoard)) {
+            console.log(gameSituation.currentPlayer.playerSymbol + " won")
+        }
         if (gameSituation.currentPlayer == x) {
             gameSituation.currentPlayer = o
         }
@@ -33,10 +75,7 @@ moveInputForm.onsubmit = function(event) {
     }
 }
 
-function gameLogic (player) {
-    currentPlayer = player
-    return {currentPlayer}
-}
+
 let gameSituation = gameLogic(x)
 
 console.log(gameSituation.currentPlayer.playerSymbol)
